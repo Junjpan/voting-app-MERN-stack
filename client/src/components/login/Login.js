39fb9a566dec446006c6b5e479e5f686855bff68
JunjPan <elisepjj@hotmail.com>
2020-01-21 20:15:56 -0800

    login form 1

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React ,{useState} from 'react';
 
 function Login() {
+    const [user,setUser]=useState('');
+    const [password,setPassword]=useState('');
+
+    function submit(e){
+        e.preventDefault();
+        console.log(user,password);
+    }
+
     return (
         <div className="login">
             <h2 className="logintitle">Member Login</h2>
+            <div>
+                <form onSubmit={submit}>
+                <label htmlFor="username">Username:</label><br/>
+                <input type="text" id="username" name="username" onChange={e=>{setUser(e.target.value)}}></input><br/>
+                <label htmlFor="password">Password:</label><br/>
+                <input type="password" id="password" name="password" onChange={e=>{setPassword(e.target.value)}}></input> <br/> <br/>              
+                <button type="submit" style={{background:"rgb(203, 233, 252)"}}>LOGIN</button>
+                </form>
+            </div>
+            <hr/>
+            <div>
+            </div>
         </div>
     )
 }
