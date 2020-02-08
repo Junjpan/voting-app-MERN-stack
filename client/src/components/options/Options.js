ae2529d3df5b4b1c278012f6364ea3f289b12c19
JunjPan <elisepjj@hotmail.com>
2020-02-07 17:17:05 -0800

    change option compoent stylesheet

--- client/src/components/options/Options.js
+++ client/src/components/options/Options.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Options(props) {
     return (
-        <ul style={{display:"flex",flexWrap:"wrap"}}>
+        <ol style={{display:"flex",flexWrap:"wrap"}}>
             {props.options.map((option,index)=>{
-            return(<li className="option" key={index}>{index+1}.{option}<button className="deleteBTN" onClick={()=>props.deleteOption(index)}>X</button></li>)
+            return(<li className="option" key={index}>{option}<button className="deleteBTN" onClick={()=>props.deleteOption(index)}>X</button></li>)
             })}
-        </ul>
+        </ol>
     )
 }
 
