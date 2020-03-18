a954d567f3d47a7f90300e07c99609189ab3e80f
JunjPan <elisepjj@hotmail.com>
2020-03-18 15:48:51 -0700

    change lint-stage.config file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import axios from 'axios';
 import Login from './components/login/Login';
 import Panel from './components/panel/Panel';
@@ -9,120 +14,168 @@ import Profile from './components/Profile/profile';
 import NewPoll from './components/NewPoll/Newpoll';
 import PrivatePanel from './components/PrivatePanel/PrivatePanel';
 
-
-
 //set up a gobal axios defaults
 axios.defaults.baseURL = 'http://localhost:5000';
 
 class App extends Component {
-
   state = {
     user: '',
-    loginStatus: false,//control the left panel view
+    loginStatus: false, //control the left panel view
     msgClosed: true,
-    privateView: false,//control the right panel view
-    message: ""
-  }
+    privateView: false, //control the right panel view
+    message: '',
+  };
 
   //check if the localstorage has user name item, if it is, the page will be still in the login mode even user is accidently close the website.
   componentDidMount() {
     const username = localStorage.getItem('user');
     //prevent people change localstorage username,double check with server,make sure has such a user.
     if (localStorage.getItem('user')) {
-      axios.get(`/api/user/${username}`)
+      axios
+        .get(`/api/user/${username}`)
         .then(res => {
           this.setState({
             loginStatus: true,
-            user: username
-          })
+            user: username,
+          });
         })
         .catch(err => {
           localStorage.clear();
-        })
+        });
     }
   }
 
-
   //if you are not using arrow function, you have to bind this when you are using this function in the render.
-  getStatus = (username) => {
-
-    if (username !== "undefined" && username !== null && username !== '') {
+  getStatus = username => {
+    if (username !== 'undefined' && username !== null && username !== '') {
       this.setState({
         user: username,
         loginStatus: true,
         message: `${username}, you are sucessully logged in! `,
-        msgClosed: false
-      })
+        msgClosed: false,
+      });
     } else {
       this.setState({
         user: '',
         loginStatus: false,
-        privateView:false,
-        message: `You are sucessully logged out! `
-      })
+        privateView: false,
+        message: `You are sucessully logged out! `,
+      });
     }
-  }
+  };
 
-  setPrivateView = (boolean) => {
+  setPrivateView = boolean => {
     this.setState({ privateView: boolean });
-  }
+  };
 
   //receive/update message
   message = (msg, status) => {
     // for the case when you use click back button
-    if (status === "undefined") {
+    if (status === 'undefined') {
       this.setState({
         message: msg,
-        msgClosed: false
-      })
+        msgClosed: false,
+      });
     } else {
       this.setState({
         message: msg,
-        msgClosed: status
-      })
+        msgClosed: status,
+      });
     }
     //when click the back button, message will not be showing.
-  }
+  };
 
   signout = () => {
-    window.location.href = "/"
+    window.location.href = '/';
 
     this.setState({
       loginStatus: false,
       message: 'You are sucessully logged out! ',
       msgClosed: false,
-      privateView:false
+      privateView: false,
     });
     localStorage.clear();
-  }
+  };
 
   render() {
-    const msgStyle = this.state.msgClosed ? { visibility: "hidden" } : { display: "flex" }
+    const msgStyle = this.state.msgClosed
+      ? { visibility: 'hidden' }
+      : { display: 'flex' };
     return (
       <Router>
         <h1 className="title">Universal Voting Board</h1>
-        {this.state.loginStatus ? <button className="LogoutBTN" onClick={this.signout}>Sign Out</button> : null}
-        <img src="https://kidstaskit.herokuapp.com/static/media/JunEBug.f67dd03a.png" className="logo" alt="logo"></img>
+        {this.state.loginStatus ? (
+          <button className="LogoutBTN" onClick={this.signout}>
+            Sign Out
+          </button>
+        ) : null}
+        <img
+          src="https://kidstaskit.herokuapp.com/static/media/JunEBug.f67dd03a.png"
+          className="logo"
+          alt="logo"
+        ></img>
         <div className="message" style={msgStyle}>
-          <p style={{ color: "red" }}>{this.state.message}</p>
-          <button style={{ width: "30px", marginTop: "10px", marginLeft: "20px" }} onClick={() => { this.setState({ msgClosed: true }) }}>X</button>
+          <p style={{ color: 'red' }}>{this.state.message}</p>
+          <button
+            style={{ width: '30px', marginTop: '10px', marginLeft: '20px' }}
+            onClick={() => {
+              this.setState({ msgClosed: true });
+            }}
+          >
+            X
+          </button>
         </div>
         <Switch>
-          <Route path="/" exact render={() => {
-            return (<div className="App" style={{ display: "flex", flexWrap: "wrap" }}>
-              {this.state.loginStatus ? <Profile status={this.getStatus} message={this.message} setPrivateView={this.setPrivateView} /> : <Login status={this.getStatus} message={this.message} />}
-              {this.state.privateView? <PrivatePanel user={this.state.user} />:<Panel message={this.message}/>}
-            </div>)
-          }} />
-          <Route path="/register" exact render={(props) => { return <Register message={this.message} {...props} /> }} />
-          {this.state.loginStatus ? <Route path="/new/:username" exact render={(props) => { return <NewPoll message={this.message} {...props} /> }} /> : <Redirect to="/" />}
+          <Route
+            path="/"
+            exact
+            render={() => {
+              return (
+                <div
+                  className="App"
+                  style={{ display: 'flex', flexWrap: 'wrap' }}
+                >
+                  {this.state.loginStatus ? (
+                    <Profile
+                      status={this.getStatus}
+                      message={this.message}
+                      setPrivateView={this.setPrivateView}
+                    />
+                  ) : (
+                    <Login status={this.getStatus} message={this.message} />
+                  )}
+                  {this.state.privateView ? (
+                    <PrivatePanel user={this.state.user} />
+                  ) : (
+                    <Panel message={this.message} />
+                  )}
+                </div>
+              );
+            }}
+          />
+          <Route
+            path="/register"
+            exact
+            render={props => {
+              return <Register message={this.message} {...props} />;
+            }}
+          />
+          {this.state.loginStatus ? (
+            <Route
+              path="/new/:username"
+              exact
+              render={props => {
+                return <NewPoll message={this.message} {...props} />;
+              }}
+            />
+          ) : (
+            <Redirect to="/" />
+          )}
           {/* In order to pass the prop object(props.match/props.history etc) to the child component, you have to use props as parameters and put {...props} in the compoent render,just like the above*/}
         </Switch>
       </Router>
     );
   }
-
 }
-
 
 export default App;
--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -1,72 +1,121 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Prompt} from 'react-router-dom';
+import { Prompt } from 'react-router-dom';
 
 function Register(props) {
-    //use props.match.params.<name> to find the params if there are params in the url
-    const [user, setUser] = useState('');
-    const [password1, setPassword1] = useState('');
-    const [password2, setPassword2] = useState('');
-    const [message,setMessage]=useState('Welcome to register center!');
-    const [isBlocking,setIsBlocking]=useState(false);
+  //use props.match.params.<name> to find the params if there are params in the url
+  const [user, setUser] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [message, setMessage] = useState('Welcome to register center!');
+  const [isBlocking, setIsBlocking] = useState(false);
 
-   
-  
-    useEffect(() => {
-        props.message(message);
-    }, [message])//put an empty array as second parameter will ensure the useEffect only run once.
-    //place a property inside the array so that whenever the message is change, the useEffect will be called once.
-    //have a little bug here, find details in the console.try to fingure out
+  useEffect(() => {
+    props.message(message);
+  }, [props.message]); //put an empty array as second parameter will ensure the useEffect only run once.
+  //place a property inside the array so that whenever the message is change, the useEffect will be called once.
+  //have a little bug here, find details in the console.try to fingure out
 
-    function submit(e) {
-        e.preventDefault();
-        setIsBlocking(false);
-        const info = {
-            username: user,
-            password: password1
-        }
-        if (password1 !== password2) {
-           setMessage("Sorry,the passwords doesn't match, please re-enter again!")
-        } else {
-            axios.post('/api/user/register', info)
-                .then(res => {
-                    props.message(res.data.message);
-                    setTimeout(()=>{
-                        window.location.href="/"
-                    },1500)
-                })
-                .catch(err => {
-                   setMessage(err.response.data.error)
-                })
-        }
+  function submit(e) {
+    e.preventDefault();
+    setIsBlocking(false);
+    const info = {
+      username: user,
+      password: password1,
+    };
+    if (password1 !== password2) {
+      setMessage("Sorry,the passwords doesn't match, please re-enter again!");
+    } else {
+      axios
+        .post('/api/user/register', info)
+        .then(res => {
+          props.message(res.data.message);
+          setTimeout(() => {
+            window.location.href = '/';
+          }, 1500);
+        })
+        .catch(err => {
+          setMessage(err.response.data.error);
+        });
     }
+  }
 
-   function goBack(){
-    props.message('',true) ; 
+  function goBack() {
+    props.message('', true);
     props.history.go(-1);
+  }
 
-   }
-
-    return (
-        <div className="register">
-            <h2 className="registertitle">Create an Account</h2>
-            <div>
-                <form onSubmit={submit}>
-                    <label htmlFor="username">Username (6-12 characters):</label><br />
-                    <input type="text" id="username" name="username" onChange={e => { setUser(e.target.value);setIsBlocking(e.target.value.length>0)  }} pattern=".{6,12}" title="6 to 12 characters are required" required></input><br /><br />
-                    <label htmlFor="password1">Password (6-18 characters):</label><br />
-                    <input type="password" id="password1" name="password1" onChange={e => { setPassword1(e.target.value);setIsBlocking(e.target.value.length>0)  }} pattern=".{6,18}" title="Password can not be less than 6 character or more than 18 characters" required></input> <br /> <br />
-                    <label htmlFor="password2">Re-enter Password:</label><br />
-                    <input type="password" id="password2" name="password2" onChange={e => { setPassword2(e.target.value);setIsBlocking(e.target.value.length>0) }} required></input> <br /> <br />
-                    <button type="submit" style={{ background: "rgb(203, 233, 252)" }}>Submit</button>
-                    <button type="button" onClick={goBack} style={{ background: "rgb(203, 233, 252)" }}>Back</button>
-               {/*props.history.go(-1) perform go back function*/}
-                </form>
-                <Prompt when={isBlocking} message={location=>`Are you sure you want to go to "${location.pathname}"`} />
-           {/*prevent people from leaving the page without submiting the data */}
-           </div>
-        </div>
-    )
+  return (
+    <div className="register">
+      <h2 className="registertitle">Create an Account</h2>
+      <div>
+        <form onSubmit={submit}>
+          <label htmlFor="username">Username (6-12 characters):</label>
+          <br />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={e => {
+              setUser(e.target.value);
+              setIsBlocking(e.target.value.length > 0);
+            }}
+            pattern=".{6,12}"
+            title="6 to 12 characters are required"
+            required
+          ></input>
+          <br />
+          <br />
+          <label htmlFor="password1">Password (6-18 characters):</label>
+          <br />
+          <input
+            type="password"
+            id="password1"
+            name="password1"
+            onChange={e => {
+              setPassword1(e.target.value);
+              setIsBlocking(e.target.value.length > 0);
+            }}
+            pattern=".{6,18}"
+            title="Password can not be less than 6 character or more than 18 characters"
+            required
+          ></input>{' '}
+          <br /> <br />
+          <label htmlFor="password2">Re-enter Password:</label>
+          <br />
+          <input
+            type="password"
+            id="password2"
+            name="password2"
+            onChange={e => {
+              setPassword2(e.target.value);
+              setIsBlocking(e.target.value.length > 0);
+            }}
+            required
+          ></input>{' '}
+          <br /> <br />
+          <button type="submit" style={{ background: 'rgb(203, 233, 252)' }}>
+            Submit
+          </button>
+          <button
+            type="button"
+            onClick={goBack}
+            style={{ background: 'rgb(203, 233, 252)' }}
+          >
+            Back
+          </button>
+          {/*props.history.go(-1) perform go back function*/}
+        </form>
+        <Prompt
+          when={isBlocking}
+          message={location =>
+            `Are you sure you want to go to "${location.pathname}"`
+          }
+        />
+        {/*prevent people from leaving the page without submiting the data */}
+      </div>
+    </div>
+  );
 }
 
 export default Register;
